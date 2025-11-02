5b82da5d4212cf2526931a5aab0dd9bba53b1448
Derik Bortoletto <derikbortolettoti@gmail.com>
2025-11-02 04:03:35 -0300

    Created ListComponent and insert into index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import ListComponent from './ListComponent';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App /> */}
+    <ListComponent />
   </React.StrictMode>
 );
 
