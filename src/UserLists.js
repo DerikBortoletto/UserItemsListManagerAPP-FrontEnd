85792646d109fe25cc74f7e5434516ac7bf14433
Derik Bortoletto <derikbortolettoti@gmail.com>
2025-11-02 05:07:34 -0300

    Connected with api and returning the lists data

--- src/UserLists.js
+++ src/UserLists.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 
 import ListComponent from "./ListComponent";
 
-export default function UserLists(props){
-    return (
-        <div>
-            <ListComponent listName={"Minha Lista"} />
-            <ListComponent listName={"Minha Lista 2"} />
-        </div>
-    )
+export default class UserLists extends Component{
+    state = { lists: null, loading: true }
+    async componentDidMount(){
+        var url = "http://localhost:8000/list/";
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data)
+        this.setState({lists: data, loading: false});
+    }
+
+    render()
+    {
+        return (
+            <div>
+                <ListComponent listName={"Minha Lista"} />
+                <ListComponent listName={"Minha Lista 2"} />
+            </div>
+        )
+    }
+    
 }
