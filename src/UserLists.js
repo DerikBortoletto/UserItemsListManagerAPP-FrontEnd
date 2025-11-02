68106a0444854abe48245cfc666bfd07f6e82807
Derik Bortoletto <derikbortolettoti@gmail.com>
2025-11-02 05:33:15 -0300

    Created listApi const to return all lists and dynamic render these lists

--- src/UserLists.js
+++ src/UserLists.js
@@ -4,7 +4,7 @@ import ListComponent from "./ListComponent";
 
 export default class UserLists extends Component{
 
-    state = { lists: null, loading: true }
+    state = { lists: [], loading: true }
 
     async componentDidMount(){
         const config = {
@@ -23,10 +23,12 @@ export default class UserLists extends Component{
 
     render()
     {
+
+        const listsApi = this.state.lists;
+
         return (
             <div>
-                <ListComponent listName={"Minha Lista"} />
-                <ListComponent listName={"Minha Lista 2"} />
+                {listsApi.map(list => <ListComponent key={list.id} listName={list.name} />)}
             </div>
         )
     }
