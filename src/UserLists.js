c3cc8cdb6e5e2055bcb7afb7773356e0e0b83436
Derik Bortoletto <derikbortolettoti@gmail.com>
2025-11-02 05:20:34 -0300

    Added token header authorization to connect backend

--- src/UserLists.js
+++ src/UserLists.js
@@ -3,10 +3,19 @@ import React, { Component } from "react";
 import ListComponent from "./ListComponent";
 
 export default class UserLists extends Component{
+
     state = { lists: null, loading: true }
+
     async componentDidMount(){
+        const config = {
+            headers: {
+                "Content-type": "application/json"
+            }
+        }
+        config.headers["Authorization"] = "Token 79395728c39511654a037e9fa10838884ed15fb5"
+
         var url = "http://localhost:8000/list/";
-        const response = await fetch(url);
+        const response = await fetch(url, config);
         const data = await response.json();
         console.log(data)
         this.setState({lists: data, loading: false});
